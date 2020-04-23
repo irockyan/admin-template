38a4d3835836c1c35f3a8a891648ee334cbffaf8
Zyan <zhaoyan@happypoker.cn>
2020-04-23 10:39:11 +0800

    feat: 优化了main.js写法

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import ElementUI from 'element-ui';
-// 从node_modules里找
-import 'element-ui/lib/theme-chalk/index.css';
+//  引入组件库
+import { Form, FormItem, Input, Button } from 'element-ui';
 import '@/assets/css/style.css';
 import http from '@/assets/js/http';
 import MyBreadcrumb from '@/components/common/brandcrumb';
 import App from './App';
 import router from './router';
+
+
+Vue.use(Form).use(FormItem).use(Input).use(Button);
 
 // 定义全局的时间过滤器
 // 格式化时间的过滤器
@@ -37,7 +39,6 @@ Vue.filter('dateFormat', (originVal) => {
   return `${y}-${m}-${d} ${hh}:${mm}:${ss}`;
 });
 
-Vue.use(ElementUI);
 // 在assets文件夹下的http.js中封装的axios的插件
 Vue.use(http);
 // 全局的面包屑组件
